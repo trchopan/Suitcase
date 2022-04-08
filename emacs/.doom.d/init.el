edd17cd0072c6908e1c00ce36b4d8d59d2de3c9e
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2022-04-09 00:30:51 +0700

    Minor format

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -45,6 +45,7 @@
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
+       format
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
@@ -53,7 +54,6 @@
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
-       format
 
        :emacs
        dired             ; making dired pretty [functional]
