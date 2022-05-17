bf4d51744c1447e49a3b7ff70ffcd773e854f7cc
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2022-05-17 13:06:41 +0700

    Fix nvim.treesitter ensure_install

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,26 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "c",
+    "typescript",
+    "javascript",
+    "haskell",
+    "rust",
+    "go",
+    "python",
+    "bash",
+    "json",
+    "lua",
+    "dart",
+    "html",
+    "css",
+    "scss",
+    "vue",
+    "vim",
+    "yaml",
+    "toml"
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
