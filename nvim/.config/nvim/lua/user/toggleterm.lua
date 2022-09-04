34e0b1ee758fc558b3fbb0adc4b38c4de8f18d71
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2022-09-04 21:18:57 +0700

    nvim: toggleterm key binding

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -4,15 +4,19 @@ if not status_ok then
 end
 
 toggleterm.setup({
+  size = 20,
   open_mapping = [[<c-\>]],
   direction = 'horizontal',
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  local k = vim.api.nvim_buf_set_keymap
-  k(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  k(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
