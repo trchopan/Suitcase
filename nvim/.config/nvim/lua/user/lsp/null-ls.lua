1bf2bb8bc0a527a68a46a82c06edb0e05a5964bc
Chop TRAN <m@chop.dev>
2023-06-09 12:05:56 +0700

    nvim: add support python

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -4,11 +4,15 @@ if not status_ok then
 end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local format = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
     sources = {
         format.prettier,
         format.brittany,
         format.cabal_fmt,
+        format.black,
+        diagnostics.mypy,
+        diagnostics.ruff,
     },
 })
