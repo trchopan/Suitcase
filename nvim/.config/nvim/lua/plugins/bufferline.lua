6b9ef78542a66711f6e5c83f9c876977fd5b7862
Chop TRAN <m@chop.dev>
2023-07-28 10:27:58 +0700

    nvim: Add moving buffer

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -10,5 +10,9 @@ return {
       desc = "Delete Buffer",
     },
     { "<leader>bK", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Other Buffers" },
+
+    -- Below moving keymap is mapped again in alacrity to sync up with browser tab move
+    { "<leader>tl", ":BufferLineMoveNext<CR>",        desc = "Move Buffer Next", silent = true },
+    { "<leader>th", ":BufferLineMovePrev<CR>",        desc = "Move Buffer Previous", silent = true },
   },
 }
