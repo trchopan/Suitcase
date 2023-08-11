587b400ac44444b14e36587e2290b967311f5a5f
Chop TRAN <m@chop.dev>
2023-08-11 12:39:29 +0700

    nvim: dont complete with <CR>

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -66,6 +66,31 @@ return {
     end,
   },
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    opts = {
+      mapping = {
+        ["<CR>"] = require("cmp").mapping({
+          i = function(fallback)
+            if require("cmp").visible() and require("cmp").get_active_entry() then
+              require("cmp").confirm({ behavior = require("cmp").ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = require("cmp").mapping.confirm({ select = true }),
+          c = require("cmp").mapping.confirm({ behavior = require("cmp").ConfirmBehavior.Replace, select = true }),
+        }),
+      },
+      preselect = require('cmp').PreselectMode.None,
+      completion = {
+        completeopt = "noselect",
+      }
+    },
+  },
+  {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
