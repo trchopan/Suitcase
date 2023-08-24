a9d4a661eef3bbc1157b2083643feb7446ee7dbd
Chop TRAN <m@chop.dev>
2023-08-24 14:24:56 +0700

    nvim: Add flutter-tools

---
+++ nvim/.config/nvim/lua/plugins/flutter.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("flutter-tools").setup({})
+
+    require("telescope").load_extension("flutter")
+  end,
+  keys = {
+    { "<leader>fp", "<cmd>Telescope flutter commands<CR>", desc = "Flutter commands" }
+  }
+}
