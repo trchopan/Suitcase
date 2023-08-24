811adad26ea03567ec791fddf99e52720cf20dba
Chop TRAN <m@chop.dev>
2023-08-24 14:25:21 +0700

    nvim: Add autocmd for shiftwidth

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dart",
+  command = "setlocal shiftwidth=2 tabstop=2"
+})
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "yaml",
+  command = "setlocal shiftwidth=4 tabstop=4"
+})
