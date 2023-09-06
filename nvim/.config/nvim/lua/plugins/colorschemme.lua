e085ef23b9a68338a0e45e5ea995d4aecce22b43
Chop TRAN <m@chop.dev>
2023-09-06 10:32:12 +0700

    nvim: Change nightfox theme

--- nvim/.config/nvim/lua/plugins/colorschemme.lua
+++ nvim/.config/nvim/lua/plugins/colorschemme.lua
@@ -2,9 +2,60 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "terafox",
+      -- colorscheme = "catppuccin",
       -- colorscheme = "tokyonight",
     },
   },
-  { "ellisonleao/gruvbox.nvim" }
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { "italic" },
+        properties = { "italic" },
+        functions = { "bold" },
+        keywords = { "italic" },
+        operators = { "bold" },
+        conditionals = { "bold" },
+        loops = { "bold" },
+        booleans = { "bold", "italic" },
+        numbers = {},
+        types = {},
+        strings = {},
+        variables = {},
+      },
+    }
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
+    priority = 1000,
+    opts = {
+      options = {
+        styles = {
+          comments = "italic",
+          conditionals = "bold",
+          constants = "NONE",
+          functions = "bold",
+          keywords = "italic",
+          numbers = "NONE",
+          operators = "bold",
+          strings = "NONE",
+          types = "NONE",
+          variables = "NONE",
+        },
+      },
+      groups = {
+        terafox = {
+          VertSplit = { bg = "bg1", fg = "#005b03" }
+        },
+        nightfox = {
+          VertSplit = { bg = "bg1", fg = "#0d4e6c" }
+        },
+      }
+    }
+  },
 }
