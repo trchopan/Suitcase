01c09662d1dfc073887ed7b6dba85943c4aeef12
Chop TRAN <m@chop.dev>
2023-09-17 08:50:10 +0700

    nvim: disable flutter

--- nvim/.config/nvim/lua/plugins/flutter.lua
+++ nvim/.config/nvim/lua/plugins/flutter.lua
@@ -1,15 +1,15 @@
-return {
-  "akinsho/flutter-tools.nvim",
-  lazy = true,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    require("flutter-tools").setup({})
-
-    require("telescope").load_extension("flutter")
-  end,
-  keys = {
-    { "<leader>fp", "<cmd>Telescope flutter commands<CR>", desc = "Flutter commands" }
-  }
-}
+-- return {
+--   "akinsho/flutter-tools.nvim",
+--   lazy = true,
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+--   config = function()
+--     require("flutter-tools").setup({})
+--
+--     require("telescope").load_extension("flutter")
+--   end,
+--   keys = {
+--     { "<leader>fp", "<cmd>Telescope flutter commands<CR>", desc = "Flutter commands" }
+--   }
+-- }
