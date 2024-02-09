936710f18e34d211055b67160264243f16707992
Chop TRAN <m@chop.dev>
2024-02-09 18:22:50 +0700

    nvim: Upgrade chatgpt

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -2,25 +2,22 @@ return {
   {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
-    commit = "24bcca7",
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "secret-openai",
-        actions_paths = {
-          vim.fn.expand("$HOME/.config/nvim/chatgpt-actions.json"),
-        },
-        openai_params = {
-          model = "gpt-4-1106-preview",
-        },
-        openai_edit_params = {
-          model = "gpt-4",
-        },
-      })
-    end,
+    opts = {
+      api_key_cmd = "secret-openai",
+      actions_paths = {
+        vim.fn.expand("$HOME/.config/nvim/chatgpt-actions.json"),
+      },
+      openai_params = {
+        model = "gpt-4-1106-preview",
+      },
+      openai_edit_params = {
+        model = "gpt-4",
+      },
+    },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
+      "nvim-telescope/telescope.nvim",
     },
     keys = {
       {
@@ -29,7 +26,7 @@ return {
         function()
           require("chatgpt").openChat()
         end,
-        desc = "ChatGPT Open Chat"
+        desc = "ChatGPT Open Chat",
       },
       {
         mode = { "v" },
@@ -37,20 +34,20 @@ return {
         function()
           require("chatgpt").edit_with_instructions()
         end,
-        desc = "ChatGPT Code with instruction"
+        desc = "ChatGPT Code with instruction",
       },
       {
         mode = { "v" },
         "<leader>pg",
         "<cmd>ChatGPTRun grammar_correction<cr>",
-        desc = "ChatGPT Grammar Correction"
+        desc = "ChatGPT Grammar Correction",
       },
       {
         mode = { "v" },
         "<leader>pc",
         "<cmd>ChatGPTRun complete_code<cr>",
-        desc = "ChatGPT Complete Code"
+        desc = "ChatGPT Complete Code",
       },
-    }
-  }
+    },
+  },
 }
