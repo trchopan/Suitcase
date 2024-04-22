3140823aa68ab88dc6119c333cd2c6b4779bd2e7
Chop TRAN <m@chop.dev>
2024-04-22 09:07:35 +0700

    nvim: Add svelte and vue console log

--- nvim/.config/nvim/lua/plugins/snipet.lua
+++ nvim/.config/nvim/lua/plugins/snipet.lua
@@ -3,9 +3,11 @@ return {
   opts = function()
     local ls = require("luasnip")
 
-    local console_log_snippet = ls.parser.parse_snippet({ trig = "clg" }, "console.log('👉', ${1:message});")
+    local console_log_snippet = ls.parser.parse_snippet({ trig = "clg" }, "console.log('👉 ${1:name}'${2:detail});")
 
     ls.add_snippets("javascript", { console_log_snippet })
     ls.add_snippets("typescript", { console_log_snippet })
+    ls.add_snippets("svelte", { console_log_snippet })
+    ls.add_snippets("vue", { console_log_snippet })
   end,
 }
