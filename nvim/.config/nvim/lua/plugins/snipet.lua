d2dd885cb1fd9e980ed21f16aa40ce3471ded85e
Chop TRAN <m@chop.dev>
2024-05-15 23:35:54 +0700

    nvim: add snippet

--- nvim/.config/nvim/lua/plugins/snipet.lua
+++ nvim/.config/nvim/lua/plugins/snipet.lua
@@ -4,10 +4,11 @@ return {
     local ls = require("luasnip")
 
     local console_log_snippet = ls.parser.parse_snippet({ trig = "clg" }, "console.log('👉 ${1:name}'${2:detail});")
+    local script_tag_snippet = ls.parser.parse_snippet({ trig = "script" }, "<script lang=\"ts\">\n$1\n</script>")
 
     ls.add_snippets("javascript", { console_log_snippet })
     ls.add_snippets("typescript", { console_log_snippet })
-    ls.add_snippets("svelte", { console_log_snippet })
-    ls.add_snippets("vue", { console_log_snippet })
+    ls.add_snippets("svelte", { console_log_snippet, script_tag_snippet })
+    ls.add_snippets("vue", { console_log_snippet, script_tag_snippet })
   end,
 }
