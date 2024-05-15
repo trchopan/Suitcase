59ac80a19f33881168faed5a70c35d95814605cd
Chop TRAN <m@chop.dev>
2024-05-15 23:35:45 +0700

    nvim: Fix wrong keymap

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -6,6 +6,5 @@ return {
   },
   keys = {
     { "<leader>d", "<cmd>DBUIToggle<cr>", { desc = "Toggle DBUI" } },
-    { "`h", "<cmd>Neotree focus reveal<cr>", { desc = "Show file in Neotree" } },
   },
 }
