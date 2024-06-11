8c0e9f595b3ec309c1c13c615b64151b1ad32f65
Chop TRAN <m@chop.dev>
2024-06-11 16:13:51 +0700

    nvim: Update packages

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = "nvim-tree/nvim-web-devicons",
+  dependencies = {"nvim-tree/nvim-web-devicons", "echasnovski/mini.bufremove"},
   keys = {
     {
       "X",
