f5ee70ea0e54a5b1f7a4af8bc17cbad9de796344
Chop TRAN <m@chop.dev>
2024-07-01 13:50:51 +0700

    nvim: Disable old symbols outline packages

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,11 @@
 return {
-  { "rcarriga/nvim-notify",      enabled = false },
-  { "ggandor/leap.nvim",         enabled = false },
-  { "ggandor/flit.nvim",         enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  { "echasnovski/mini.pairs",    enabled = false },
-  { "folke/flash.nvim",          enabled = false },
-  { "folke/noice.nvim",          enabled = false },
+  { "rcarriga/nvim-notify",           enabled = false },
+  { "ggandor/leap.nvim",              enabled = false },
+  { "ggandor/flit.nvim",              enabled = false },
+  { "echasnovski/mini.surround",      enabled = false },
+  { "echasnovski/mini.pairs",         enabled = false },
+  { "folke/flash.nvim",               enabled = false },
+  { "folke/noice.nvim",               enabled = false },
+  { "folke/noice.nvim",               enabled = false },
+  { "simrat39/symbols-outline.nvim",  enabled = false },
 }
