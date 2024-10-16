2148839eb4a5a54e923057397037fcc31b3ef485
Chop TRAN <m@chop.dev>
2024-10-16 14:45:28 +0700

    nvim: Remove version lock of codeium

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,4 +1,7 @@
-return {
-  "Exafunction/codeium.nvim",
-  commit = "937667b2cadc7905e6b9ba18ecf84694cf227567",
-}
+-- Lock commit because of bug. Remove after fix.
+--
+-- return {
+--   "Exafunction/codeium.nvim",
+--   commit = "937667b2cadc7905e6b9ba18ecf84694cf227567",
+-- }
+return {}
