ff33c9de202ba3caec49d2baada0f7b46d0fc268
Chop TRAN <m@chop.dev>
2025-09-16 09:11:15 +0700

    nvim: snippet for elixir

--- nvim/.config/nvim/lua/plugins/snipet.lua
+++ nvim/.config/nvim/lua/plugins/snipet.lua
@@ -3,30 +3,24 @@ return {
   opts = function()
     local ls = require("luasnip")
 
-    local console_log_snippet = ls.parser.parse_snippet({ trig = "clg" }, "console.log('👉 ${1:name}'${2:detail});")
-    local script_tag_snippet = ls.parser.parse_snippet({ trig = "script" }, '<script lang="ts">\n$1\n</script>')
+    local console_log_snippet = ls.parser.parse_snippet({ trig = "clg" }, "console.log('👉 ${1:name}', ${2:detail});")
 
     ls.add_snippets("javascript", { console_log_snippet })
     ls.add_snippets("typescript", { console_log_snippet })
-    -- ls.add_snippets("svelte", { console_log_snippet, script_tag_snippet })
-    -- ls.add_snippets("vue", { console_log_snippet, script_tag_snippet })
 
-    -- vim.keymap.set({ "i" }, "<C-K>", function()
-    --   ls.expand()
-    -- end, { silent = true })
-    --
-    -- vim.keymap.set({ "i", "s" }, "<C-L>", function()
-    --   ls.jump(1)
-    -- end, { silent = true })
-    --
-    -- vim.keymap.set({ "i", "s" }, "<C-J>", function()
-    --   ls.jump(-1)
-    -- end, { silent = true })
-    --
-    -- vim.keymap.set({ "i", "s" }, "<C-E>", function()
-    --   if ls.choice_active() then
-    --     ls.change_choice(1)
-    --   end
-    -- end, { silent = true })
+    local if_block_snippet =
+      ls.parser.parse_snippet({ trig = "ifdo" }, "<%= if ${1:condition} do %>\n  ${2:content}\n<% end %>")
+
+    local fordo_block_snippet = ls.parser.parse_snippet(
+      { trig = "fordo" },
+      "<%= for ${1:item} <- ${2:enumerable} do %>\n  ${3:content}\n<% end %>"
+    )
+    local elixir_snippets = { if_block_snippet, fordo_block_snippet }
+    ls.add_snippets("heex", elixir_snippets)
+    ls.add_snippets("ex", elixir_snippets)
+
+    local script_tag_snippet = ls.parser.parse_snippet({ trig = "script" }, '<script lang="ts">\n$1\n</script>')
+    ls.add_snippets("svelte", { console_log_snippet, script_tag_snippet })
+    ls.add_snippets("vue", { console_log_snippet, script_tag_snippet })
   end,
 }
