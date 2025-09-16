d7663dc5d49f9015964671e20a151f4aa4fdb7d3
Chop TRAN <m@chop.dev>
2025-09-16 09:10:40 +0700

    nvim: All Marks are global

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -14,3 +14,16 @@ opt.foldmethod = "indent"
 opt.foldlevelstart = 20
 opt.iskeyword = opt.iskeyword + "-"
 opt.linebreak = true
+
+for c = string.byte('a'), string.byte('z') do
+  local lower = string.char(c)
+  local upper = string.char(c):upper()
+
+  -- Remap setting a mark
+  vim.keymap.set('n', 'm'..lower, 'm'..upper, { noremap = true, silent = true })
+  -- Remap jumping to a mark (line)
+  vim.keymap.set('n', "'"..lower, "'"..upper, { noremap = true, silent = true })
+  -- Remap jumping to a mark (exact place)
+  vim.keymap.set('n', '`'..lower, '`'..upper, { noremap = true, silent = true })
+end
+
