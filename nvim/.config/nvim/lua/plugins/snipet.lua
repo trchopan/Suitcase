c49465f534e7dadc4e117325696bc6a11c648520
Chop TRAN <m@chop.dev>
2025-10-05 00:15:02 +0700

    nvim: Fix elixir snippets

--- nvim/.config/nvim/lua/plugins/snipet.lua
+++ nvim/.config/nvim/lua/plugins/snipet.lua
@@ -15,9 +15,11 @@ return {
       { trig = "fordo" },
       "<%= for ${1:item} <- ${2:enumerable} do %>\n  ${3:content}\n<% end %>"
     )
+
+    local tap_snippet = ls.parser.parse_snippet({ trig = "tap" }, 'tap(&IO.inspect(&1${1:property}, label: "label"))')
     local elixir_snippets = { if_block_snippet, fordo_block_snippet }
     ls.add_snippets("heex", elixir_snippets)
-    ls.add_snippets("ex", elixir_snippets)
+    ls.add_snippets("elixir", vim.list_extend({ tap_snippet }, elixir_snippets))
 
     local script_tag_snippet = ls.parser.parse_snippet({ trig = "script" }, '<script lang="ts">\n$1\n</script>')
     ls.add_snippets("svelte", { console_log_snippet, script_tag_snippet })
