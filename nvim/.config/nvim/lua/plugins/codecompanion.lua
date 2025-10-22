ffe80f3cddf397954d65b483d3476802345cc717
Chop TRAN <m@chop.dev>
2025-10-22 15:14:00 +0700

    nvim: Add mcphub

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -214,10 +214,21 @@ return {
         },
       },
     },
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+    },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
   },
   keys = {
     {
