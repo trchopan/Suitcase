715ba2933c0ab0962820f882f59947349195659c
Chop TRAN <m@chop.dev>
2025-12-03 10:57:56 +0700

    nvim: gwd key for neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -10,8 +10,8 @@ return {
     lazy = false, -- neo-tree will lazily load itself
     keys = function()
       return {
-        { "<leader>\\", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neotree" } },
-        { "`h", "<cmd>Neotree focus reveal<cr>", { desc = "Show file in Neotree" } },
+        { "<leader>\\", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
+        { "`h", "<cmd>Neotree focus reveal<cr>", desc = "Show file in Neotree" },
       }
     end,
     opts = {
@@ -148,5 +148,34 @@ return {
         },
       })
     end,
+    keys = {
+      {
+        mode = { "n" },
+        "gwd",
+        function()
+          -- Step 1: Create a mark at the current cursor position
+          vim.cmd("mark M")
+
+          -- Step 2: Go to the definition at the current cursor
+          vim.lsp.buf.definition()
+
+          -- Step 3: Use window-picker to select another window
+          local picker = require("window-picker")
+          local picked_window = picker.pick_window()
+          if picked_window then
+            vim.api.nvim_set_current_win(picked_window)
+          else
+            -- Open a new split window if no window is selected
+            vim.cmd("vsplit")
+            local new_window = vim.api.nvim_get_current_win()
+            vim.api.nvim_set_current_win(new_window)
+          end
+
+          -- Step 4: Go to the saved mark
+          vim.cmd("normal! `M")
+        end,
+        desc = "Goto Definition in a window picker",
+      },
+    },
   },
 }
