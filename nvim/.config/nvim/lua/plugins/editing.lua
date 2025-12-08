296e5d20fed86c51c3f7d39a13a11892a3a2f78a
Chop TRAN <m@chop.dev>
2025-12-08 16:52:59 +0700

    nvim: Add search and replace

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -61,4 +61,26 @@ return {
       }
     end,
   },
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = { "GrugFar", "GrugFarWithin" },
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          })
+        end,
+        mode = { "n", "x" },
+        desc = "Search and Replace",
+      },
+    },
+  },
 }
