685f5cb195267b66d6e257371503b44f83ab2873
Chop TRAN <m@chop.dev>
2025-12-08 16:52:37 +0700

    nvim: Fix volar path to ts_ls

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -66,3 +66,24 @@ vim.diagnostic.config({
     },
   },
 })
+
+-- Ensure 'vue-language-server' is installed via Mason
+-- Then, manually locate the Volar plugin path:
+local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server")
+local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+
+-- Configure tsserver (TypeScript) to load the Vue plugin
+vim.lsp.config("ts_ls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_plugin_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "vue" },
+})
+
+vim.lsp.enable("ts_ls")
