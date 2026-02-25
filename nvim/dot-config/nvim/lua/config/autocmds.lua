01665062a572033476689453775130f3c98872b1
Chop TRAN <m@chop.dev>
2026-02-25 08:36:26 +0700

    nvim: wrap spell for yaml

--- nvim/dot-config/nvim/lua/config/autocmds.lua
+++ nvim/dot-config/nvim/lua/config/autocmds.lua
@@ -97,9 +97,10 @@ vim.api.nvim_create_autocmd("FileType", {
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("wrap_spell"),
-  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  pattern = { "text", "typst", "gitcommit", "markdown", "yaml" },
   callback = function()
     vim.opt_local.wrap = true
+    vim.opt_local.breakindent = true
     vim.opt_local.spell = true
   end,
 })
