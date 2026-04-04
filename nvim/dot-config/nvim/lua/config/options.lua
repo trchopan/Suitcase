5525433ec657b0a2ff1d5bf9831ba944d4e3ae54
Chop TRAN <m@chop.dev>
2026-04-04 12:45:36 +0700

    nvim: read copilot toggle from NVIM_COPILOT_ENABLED

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -18,4 +18,5 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wrapscan = true
 
-vim.g.copilot_enabled = 0
+local copilot_env = vim.env.NVIM_COPILOT_ENABLED
+vim.g.copilot_enabled = (copilot_env == "1") and 1 or 0
